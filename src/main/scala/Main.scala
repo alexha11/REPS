fd62ffd56d7c9b34370e5c5ad8a8b5f7c8807263
Pouya Amiri <pooyaamiri@protonmail.com>
2023-05-09 01:14:15 +0300

    Added data

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,5 +5,8 @@ object Main extends App {
   val HydroCSV = CSVReader.open(new File("./src/main/scala/Hydro.csv"))
   val SolarCSV = CSVReader.open(new File("./src/main/scala/Solar.csv"))
   val WindCSV = CSVReader.open(new File("./src/main/scala/Wind.csv"))
+  val HydroData = HydroCSV.all()
+  val SolarData = SolarCSV.all()
+  val WindData = WindCSV.all()
   println("Hello, World!")
 }
