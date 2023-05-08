328988e6e706a439a0a8cb2dfee693e92361a319
Pouya Amiri <pooyaamiri@protonmail.com>
2023-05-09 01:10:52 +0300

    Added data

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,9 @@
+import com.github.tototoshi.csv._
+import java.io.File
+
 object Main extends App {
+  val HydroCSV = CSVReader.open(new File("./src/main/scala/Hydro.csv"))
+  val SolarCSV = CSVReader.open(new File("./src/main/scala/Solar.csv"))
+  val WindCSV = CSVReader.open(new File("./src/main/scala/Wind.csv"))
   println("Hello, World!")
 }
