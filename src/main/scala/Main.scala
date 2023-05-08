a417a083b0b0a7c053d17440d184c2958b4ed64d
Pouya Amiri <pooyaamiri@protonmail.com>
2023-05-09 01:44:28 +0300

    Rework

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -8,5 +8,35 @@ object Main extends App {
   val HydroData = HydroCSV.all()
   val SolarData = SolarCSV.all()
   val WindData = WindCSV.all()
-  println("Hello, World!")
+
+  var running = true
+
+  while (running) {
+    println("Menu:")
+    println("1. View Hydro Data")
+    println("2. View Solar Data")
+    println("3. View Wind Data")
+    println("4. Exit")
+
+    print("Please enter your choice: ")
+    val choice = scala.io.StdIn.readInt()
+
+    choice match {
+      case 1 =>
+        println("Hydro Data:")
+        HydroData.foreach(println)
+      case 2 =>
+        println("Solar Data:")
+        SolarData.foreach(println)
+      case 3 =>
+        println("Wind Data:")
+        WindData.foreach(println)
+      case 4 =>
+        running = false
+        println("Goodbye!")
+      case _ =>
+        println("Invalid choice. Please try again.")
+    }
+    println()
+  }
 }
